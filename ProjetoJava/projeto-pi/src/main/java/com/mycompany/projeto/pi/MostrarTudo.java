6790410f5d399cf225b82bc30e002bf966590285
allanlopesbandtec <allan.lopes@bandtec.com.br>
2020-04-27 14:34:50 -0300

    Comentado e adicionada a temperatura do CPU

---
+++ ProjetoJava/projeto-pi/src/main/java/com/mycompany/projeto/pi/MostrarTudo.java
@@ -0,0 +1,82 @@
+package com.mycompany.projeto.pi;
+
+import java.util.ArrayList;
+import java.util.List;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.Sensors;
+//import oshi.software.os.NetworkParams;
+
+public class MostrarTudo {
+   
+    SystemInfo systemInfo = new SystemInfo();
+    //Interface com informações sobre o sistema
+        
+        HardwareAbstractionLayer hardwareInfo = systemInfo.getHardware();
+        //Interface com informações sobre os componentes apartir dessa interface vamos 
+        //usar classes para pegar informações de cada componente
+        
+        CentralProcessor cpu = hardwareInfo.getProcessor();
+        //Classe de abstração do CPU
+        
+        Sensors sensor = hardwareInfo.getSensors();
+        //Informações dos sensores (Usaremos a temperatura)
+
+   
+        
+        GlobalMemory GM = hardwareInfo.getMemory();
+        //Informações da memória
+       
+        //NetworkParams net = systemInfo.getOperatingSystem().getNetworkParams();
+        //Informações placa de rede e sistema (Ip etc)
+       
+        
+        private String memoriaDisponivel = "", memoriaTotal = "", memoriaFisica = "";
+                
+        private Double temperaturaCpu;
+        
+        private List freqCpu = new ArrayList<>(); 
+
+         
+    
+        public void verificarMemoriaDisponivel(){
+        memoriaDisponivel = String.format("Memória disponivel: %.2f", Double.valueOf(GM.getAvailable()));    
+        
+              //double recebelonge = (double) variavelLong
+        
+          
+        freqCpu.add(cpu.getCurrentFreq());
+           
+                  
+        
+        temperaturaCpu =  sensor.getCpuTemperature();
+               
+        //memoriaTotal = String.format("Memória total: %.2f", Double.valueOf(GM.getTotal()));   
+               
+        //memoriaFisica = String.format("Memória fisica: %.2f", Double.valueOf(GM.getPhysicalMemory()));          
+        }
+
+    public Double getTemperaturaCpu() {
+        return temperaturaCpu;
+    }
+
+    public String getMemoriaTotal() {
+        return memoriaTotal;
+    }
+
+    public String getMemoriaFisica() {
+        return memoriaFisica;
+    }
+        
+    public String getMemoriaDisponivel() {
+        return memoriaDisponivel;   
+    }
+    
+    public List getFreqCpu() {
+        return freqCpu;
+    }
+}
+
+
