6fa5a81828914138c5b483ad51a750363941655a
italo-andrade-99 <italo.andrade@bandtec.com.br>
2020-05-26 21:44:29 -0300

    Logs funcionando e sendo nomeados por data

--- ProjetoJava/projeto-pi/src/main/java/Conexao/Logs.java
+++ ProjetoJava/projeto-pi/src/main/java/Conexao/Logs.java
@@ -6,36 +6,43 @@ import java.io.IOException;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 /**
  *
  * @author Italo
  */
 public class Logs {
-    
+    private ArrayList <String> registro = new ArrayList();
     public void gravarLog(String error, String sistema){
+        
+        //Data atual do computador
+        DateFormat dataAtual = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        DateFormat nomearArquivo = new SimpleDateFormat("yyyyMMdd");
+        Date date = new Date();
         
         try {
         //Criando o arquivo de log
-        File acesso = new File("C:\\Users\\Italo\\desktop\\TotensTech_Logs.txt");            
+        File acesso = new File("C:\\Users\\Italo\\desktop\\"+nomearArquivo.format(date)+"_TotensTech_Logs.txt");
         FileWriter arquivo = new FileWriter(acesso);
-
+        
         acesso.setReadable(true);
         acesso.setWritable(true);
                
-        //Data atual do computador
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        Date date = new Date();
+       
         
-        String registro = String.format("%s  ||  %s  ||  %s", dateFormat.format(date), sistema, error);
+        registro.add(String.format("%s  ||  %s  ||  %s \n", dataAtual.format(date), sistema, error));
         
-        arquivo.append(registro);
+        for(String contador : registro){
+            System.out.println(contador);
+            arquivo.append(contador);
+        }
         
         arquivo.flush();
         arquivo.close();
         
         } catch (IOException e) {
-            System.out.println("Erro desconhecido, entre em contato com a central de atendimento. "+ e);
+            System.out.println("Erro desconhecido, entre em contato com a central de atendimento. \nCodigo do erro: "+ e);
         }
         
     }
