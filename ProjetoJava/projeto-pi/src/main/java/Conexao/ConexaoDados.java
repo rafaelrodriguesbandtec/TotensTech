68a1f8c3f7cd2209bb4ded42dc67a9807036c3ce
Rafael de Oliveira Rodrigues <rorodrigues@stefanini.com>
2020-06-16 12:17:37 -0300

    conexao bando de dados azure

--- ProjetoJava/projeto-pi/src/main/java/Conexao/ConexaoDados.java
+++ ProjetoJava/projeto-pi/src/main/java/Conexao/ConexaoDados.java
@@ -12,10 +12,12 @@ import org.apache.commons.dbcp2.BasicDataSource;
 public class ConexaoDados {
     
     private static final String DRIVE = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private static final String URL = "jdbc:sqlserver://svrtotenstech.database.windows.net:1433";
-    private static final String USER = "localadmin@svrtotenstech";
+    private static final String HOSTNAME =  "svrtotenstech.database.windows.net";
+    private static final String DBNAME =  "bdTotensTech";
+    private static final String USER = "localadmin";
     private static final String PASS = "#Gfgrupo5b";
-    
+    private static final String URL = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30", HOSTNAME, DBNAME, USER, PASS);
+
     //private static final String DRIVE = "com.mysql.cj.jdbc.Driver";
     //private static final String URL = "jdbc:mysql://localhost:3306/totenstech?useTimezone=true&serverTimezone=UTC";
     //private static final String USER = "root";
