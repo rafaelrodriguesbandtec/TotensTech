a87ac987bfebedabdaaac7ef8c6d994e6e1f0fd5
Rafael de Oliveira Rodrigues <rorodrigues@stefanini.com>
2020-06-28 23:58:44 -0300

    ajustes dash

--- ProjetoSite/public/js/funcoes.js
+++ ProjetoSite/public/js/funcoes.js
@@ -4,7 +4,7 @@
 var razaoSocial;
 var IdEmpresa;
 
-  //está função coleta os dados para a dashboard
+//está função coleta os dados para a dashboard
 
 function carregarDados(param) {
   var controle = 0;
@@ -120,18 +120,18 @@ function carregarDados(param) {
         chart2.update();
         chart3.update();
 
-        
+
         for (i = 0; i < resposta.length; i++) {
           var registro = resposta[i];
           if (registro.fkToten == param && controle <= 4) {
-            
+
             chart3.data.labels.push(registro.momento_grafico);
             chart3.data.datasets[0].data.push(registro.LeituraCpu.toFixed(2));
             chart3.data.datasets[1].data.push(registro.LeituraMemoria.toFixed(2));
             chart3.data.datasets[2].data.push(registro.TemperaturaCpu.toFixed(2));
             cpuDiv.innerHTML = `${registro.LeituraCpu.toFixed(0)}%`;
             memoriaDiv.innerHTML = `${registro.LeituraMemoria.toFixed(0)}%`;
-           
+
             tempDiv.innerHTML = `${registro.TemperaturaCpu.toFixed(0)}°C`;
             if (registro.TemperaturaCpu > 75) {
               document.getElementById(`tempDiv`).style = "color: red !important;";
@@ -139,18 +139,18 @@ function carregarDados(param) {
             if (registro.LeituraMemoria > 75) {
               document.getElementById(`memoriaDiv`).style = "color: red !important;";
             }
-            if (controle == 0) {
-             
+            if (controle == 4) {
+
               var calcResto = (100 - registro.LeituraDisco).toFixed(2);
               chart2.data.datasets[0].data.push(registro.LeituraDisco.toFixed(2));
               chart2.data.datasets[0].data.push(calcResto);
               discoDiv.innerHTML = `${registro.LeituraDisco.toFixed(0)}%`;
-           
+
               if (registro.LeituraDisco > 75) {
                 document.getElementById(`discoDiv`).style = "color: red !important;";
               }
-             
-             
+
+
             }
             controle++;
           }
@@ -180,37 +180,38 @@ function carregarDados(param) {
 
 // Está função seta as cores nos cards de acordo com seu registro 
 function plotarCards(parametro) {
+  var indiceCard = 0;
 
   document.getElementById(`${parametro}Card`).style = "background-color: red !important";
-  var indiceCard = 0;
   fetch('/empresas/ultimas', { cache: 'no-store' }).then(function (response) {
     if (response.ok) {
 
 
       response.json().then(function (resposta) {
 
-        
+
 
         resposta.reverse();
 
         for (i = 0; i < resposta.length; i++) {
           var registro = resposta[i];
-          if (registro.fkToten == parametro && indiceCard < 1) {
-            if (registro.LeituraDisco > 75 || registro.LeituraMemoria > 75 || registro.TemperaturaCpu > 75) {
-              document.getElementById(`${parametro}Card`).style = "background-color: red !important";
+
+          if (registro.fkToten == parametro) {
+            if (indiceCard == 4) {
+
+              if (registro.LeituraDisco > 75 || registro.LeituraMemoria > 75 || registro.TemperaturaCpu > 75) {
+                document.getElementById(`${parametro}Card`).style = "background-color: red !important";
+              }
+              else if (registro.LeituraDisco < 65 && registro.TemperaturaCpu < 40 && registro.LeituraMemoria < 45) {
+                document.getElementById(`${parametro}Card`).style = "background-color: green !important";
+              } else {
+                document.getElementById(`${parametro}Card`).style = "background-color: #ffc107 !important";
+              }
 
             }
-            else if (registro.LeituraDisco < 65 && registro.TemperaturaCpu < 40 && registro.LeituraMemoria < 55) {
-              document.getElementById(`${parametro}Card`).style = "background-color: green !important";
-            }else {
-              document.getElementById(`${parametro}Card`).style = "background-color: #ffc107 !important";
-            }
-
-
-
             indiceCard++;
-            break;
           }
+
         }
 
 
@@ -243,7 +244,7 @@ function carregarMaquinas() {
     if (response.ok) {
       response.json().then(function (resposta) {
 
-        
+
 
         resposta.reverse();
 
@@ -308,42 +309,21 @@ function puxarMaquina(params) {
   fetch('/empresas/totens', { cache: 'no-store' }).then(function (response) {
     if (response.ok) {
       response.json().then(function (resposta) {
-
-        //console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
-
         resposta.reverse();
-
-
-
 
         for (i = 0; i < resposta.length; i++) {
 
           if (i == params) {
             var registro = resposta[i];
 
-
-
             fabricanteTotem.innerHTML = registro.Fabricante;
             modTotem.innerHTML = registro.Modelo;
             processadorTotem.innerHTML = registro.CpuToten;
             memoriaTotem.innerHTML = registro.Memoria;
             hdTotem.innerHTML = registro.EspacoEmDisco;
-            
-
-
-
-
-
 
           }
-
-
         }
-
-
-
-
-
       });
     } else {
       console.error('Nenhum dado encontrado ou erro na API');
@@ -358,7 +338,7 @@ function puxarMaquina(params) {
 function redirecionar_home() {
   window.location.href = '../index.html';
 }
-function verificarAutenticacaoUsuario(){
+function verificarAutenticacaoUsuario() {
   IdEmpresa = sessionStorage.IdEmpresa_meuapp;
 
   fkEmpresa.value = IdEmpresa;
@@ -370,7 +350,7 @@ function verificarAutenticacaoUsuario(){
 
 function verificar_autenticacao() {
   razaoSocial = sessionStorage.razaoSocialApp; //localStorage.getItem('RazaoSocial');
-  
+
   IdEmpresa = sessionStorage.IdEmpresa_meuapp;
 
 
@@ -401,7 +381,7 @@ function validar_sessao() {
   fetch(`/empresas/sessao/${razaoSocial}`, { cache: 'no-store' })
     .then(resposta => {
 
-     
+
 
       if (resposta.ok) {
         resposta.text().then(texto => {
